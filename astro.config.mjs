f952c201111c4340dc8b5efb7c532824c4018904
mehan05 <mehanmehan6@gmail.com>
2026-05-12 17:12:17 +0530

    feat: worked in clerk integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import clerk from '@clerk/astro';
+import node from '@astrojs/node';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [clerk()],
+  adapter: node({
+    mode: 'standalone',
+  }),
+  output: 'server',
+});
