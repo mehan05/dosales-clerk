42792d5849050b0e12ca606324cddb6d18c72548
mehan05 <mehanmehan6@gmail.com>
2026-05-13 11:42:09 +0530

    fix: deployment error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import clerk from '@clerk/astro';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [clerk()],
-  adapter: node({
-    mode: 'standalone',
-  }),
+  adapter: vercel(),
   output: 'server',
 });
