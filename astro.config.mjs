cd1bc945d749cb00a861afabcabf3490c957cfb0
mehan05 <mehanmehan6@gmail.com>
2026-05-13 11:46:14 +0530

    fix: build error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import clerk from '@clerk/astro';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
